ac505234bcf8b667a4b45fa46b732dcd9e45f6fa
Kathrin Rohrmeier <theliteratecondition@gmail.com>
2023-04-08 13:17:42 -0500

    add modal, animate icon, style buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Dialog } from '@reach/dialog';
+import '@reach/dialog/styles.css';
+
+import { ReactComponent as BookshelfLogo } from './images/bookshelfLogo.svg';
 import './App.css';
 
 function App() {
+  const [openModal, setOpenModal] = useState('');
+  const open = (buttonPurpose = '') => setOpenModal(buttonPurpose);
+  const close = () => setOpenModal('');
+
+  const onSVGMouseOver = (animationID) => {
+    const animateElement = document.getElementById(animationID);
+    animateElement.beginElement();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <main>
+        <header className='app-header'>
+          <div className='logo-div'>
+            <BookshelfLogo onMouseOver={() => onSVGMouseOver('bookshelf')} />{' '}
+          </div>
+          <h1>My BookS(h)elf</h1>
+        </header>
+
+        <button className='open-modal primary' onClick={() => open('login')}>
+          Login
+        </button>
+
+        <button
+          className='open-modal secondary'
+          onClick={() => open('register')}
         >
-          Learn React
-        </a>
-      </header>
+          Register
+        </button>
+
+        <Dialog
+          onDismiss={close}
+          isOpen={openModal === 'login' || openModal === 'register'}
+        >
+          <button className='close-modal' onClick={close}>
+            close
+          </button>
+          <p>You clicked a button to {openModal}.</p>
+        </Dialog>
+      </main>
     </div>
   );
 }
